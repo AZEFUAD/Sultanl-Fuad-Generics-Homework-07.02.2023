f065ea1188ee43510f402e00a0e245b77cc339dc
AZEFUAD <fuadkhs@code.edu.az>
2023-03-05 23:06:56 +0400

    Your file successfully committed.

---
+++ Sultanlı Fuad Generics Homework 07.02.2023/Sultanlı Fuad Generics Homework 07.02.2023/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Sultanlı_Fuad_Generics_Homework_07._02._2023
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GetDetails<string>("Orange");
+            Swap<int>(8, 4);
+
+        }
+
+        static void GetDetails<T>(T type)
+        {
+            Console.WriteLine(type);
+        }
+
+        static void Swap<T>(T a, T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
